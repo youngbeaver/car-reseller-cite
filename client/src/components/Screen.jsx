f1f504381d44a72240df6bc2d4e0c9ee938847c8
Daniil Medvedev <101728909+mrbigmoney62@users.noreply.github.com>
2023-10-16 18:10:47 +0300

    maket 1/2

--- client/src/components/Screen.jsx
+++ client/src/components/Screen.jsx
@@ -1,10 +1,56 @@
 import React from "react";
 import Toolbar from "./ui/Toolbar.jsx";
+import styles from "./Screen.module.css";
+import vesta from "./vesta.png";
 
 const Screen = () => {
   return (
     <div>
       <Toolbar />
+      <div>
+        <div className={styles.stroke}>
+          <p>Автоподбор - </p>
+          <p>Это сложный механизм</p>
+        </div>
+        <img className={styles.img} src={vesta} alt="" />
+      </div>
+      <p>Что такое качество?</p>
+      <img src="" alt="" />
+      <img src="" alt="" />
+      <img src="" alt="" />
+      <p>Своевременная консультация</p>
+      <p>Дотошность к каждой мелочи</p>
+      <p>Готовность нести ответственность за свою работу</p>
+      <div>
+        <img src="" alt="" />
+        <p>Mazda 3 2008 г.</p>
+        <img src="" alt="" />
+        <p>Отзыв</p>
+        <div>
+          <p>Долго искал и т.д.</p>
+        </div>
+      </div>
+      <div>
+        <img src="" alt="" />
+        <p>Mazda 3 2008 г.</p>
+        <img src="" alt="" />
+        <p>Отзыв</p>
+        <div>
+          <p>Долго искал и т.д.</p>
+        </div>
+      </div>
+      <div>
+        <img src="" alt="" />
+        <p>Mazda 3 2008 г.</p>
+        <img src="" alt="" />
+        <p>Отзыв</p>
+        <div>
+          <p>Долго искал и т.д.</p>
+        </div>
+      </div>
+      <div>
+        <div></div>
+      </div>
     </div>
   );
 };
--- client/src/components/ui/Toolbar.jsx
+++ client/src/components/ui/Toolbar.jsx
@@ -6,6 +6,12 @@ const Toolbar = () => {
     <div>
       <div className={styles.header}>
         <p className={styles.name}>IVANOV</p>
+        <p className={styles.number}>+7 (999) 999 99 99</p>
+        <p className={styles.number_information}>(Скажите, что вы с сайта и получите скидку!)</p>
+        <button>Услуги</button>
+        <button>Обо мне</button>
+        <button>Гарантии</button>
+        <button>Контакты</button>
       </div>
     </div>
   );
