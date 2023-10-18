e4ccbc9e80300d77fbcea5bf29029ee4effba315
Daniil Medvedev <101728909+youngbeaver@users.noreply.github.com>
2023-10-18 19:17:00 +0300

    add text style

--- client/src/components/Screen.jsx
+++ client/src/components/Screen.jsx
@@ -8,60 +8,65 @@ const Screen = () => {
     <div>
       <Toolbar />
       <div>
-        <div className={styles.stroke}>
-          <p className={styles.stroke_header}>Автоподбор - </p>
-          <p className={styles.stroke_fulltext}>Это сложный механизм</p>
+        <div>
+          <div className={styles.stroke}>
+            <p className={styles.stroke_header}>Автоподбор - </p>
+            <p className={styles.stroke_fulltext}>Это сложный механизм</p>
+          </div>
+          <img className={styles.img} src={process.env.PUBLIC_URL + "img/header/vesta.png"} alt="" />
         </div>
-        <img className={styles.img} src={process.env.PUBLIC_URL + "img/header/vesta.png"} alt="" />
-      </div>
-      <p className={styles.quality}>Что такое качество?</p>
-      <img className={styles.svg_support} src={process.env.PUBLIC_URL + "img/svg/support.svg"} alt="" />
-      <img className={styles.svg_meticulousness} src={process.env.PUBLIC_URL + "img/svg/meticulousness.svg"} alt="" />
-      <img className={styles.svg_carry} src={process.env.PUBLIC_URL + "img/svg/carry.svg"} alt="" />
-      <p className={styles.p_timely}>Своевременная консультация</p>
-      <p className={styles.p_meticulousness}>Дотошность к каждой мелочи</p>
-      <p className={styles.p_carry}>Готовность нести ответственность за свою работу</p>
 
-      <div className={styles.feedback_block}>
-        <div className={styles.feedback_first}>
-          <img className={styles.feedback_img} src={process.env.PUBLIC_URL + "img/feedback/mazda.png"} alt="" />
-          <p className={styles.car_information}>Mazda 3 2008 г.</p>
-          <img className={styles.feedback_avatar} src={process.env.PUBLIC_URL + "img/avatar/feedback.png"} alt="" />
-          <p>Отзыв</p>
-          <div className={styles.feedback_div}>
-            <p>Долго искал и т.д.</p>
-          </div>
-        </div>
-        <div className={styles.feedback_second}>
-          <img className={styles.feedback_img} src={process.env.PUBLIC_URL + "img/feedback/mazda.png"} alt="" />
-          <p>Mazda 3 2008 г.</p>
-          <img className={styles.feedback_avatar} src={process.env.PUBLIC_URL + "img/avatar/feedback.png"} alt="" />
-          <p>Отзыв</p>
-          <div className={styles.feedback_div}>
-            <p>Долго искал и т.д.</p>
-          </div>
-        </div>
-        <div className={styles.feedback_third}>
-          <img className={styles.feedback_img} src={process.env.PUBLIC_URL + "img/feedback/mazda.png"} alt="" />
-          <p>Mazda 3 2008 г.</p>
-          <img className={styles.feedback_avatar} src={process.env.PUBLIC_URL + "img/avatar/feedback.png"} alt="" />
-          <p>Отзыв</p>
-          <div className={styles.feedback_div}>
-            <p>Долго искал и т.д.</p>
-          </div>
-        </div>
-      </div>
+        <p className={styles.quality}>Что такое качество?</p>
+        <img className={styles.svg_support} src={process.env.PUBLIC_URL + "img/svg/support.svg"} alt="" />
+        <img className={styles.svg_meticulousness} src={process.env.PUBLIC_URL + "img/svg/meticulousness.svg"} alt="" />
+        <img className={styles.svg_carry} src={process.env.PUBLIC_URL + "img/svg/carry.svg"} alt="" />
+        <p className={styles.p_timely}>Своевременная консультация</p>
+        <p className={styles.p_meticulousness}>Дотошность к каждой мелочи</p>
+        <p className={styles.p_carry}>Готовность нести ответственность за свою работу</p>
 
-      <Information></Information>
-      <button>
-        <p>Оставить заявку для консультации</p>
-      </button>
-      <div className={styles.bottom}>
-        <p className={styles.p_bottom}>
-          Сайт является местом содержания краткой информации о услугах Иванова Ивана Ивановича и не содержит полной информации.
-          Для консультации вы сможете уточнить по номеру - +7 (999) 999-99-99
-        </p>
-        <p>При обнаружении проблем с сайтом сообщайте по почте - gmail@gmail.com</p>
+        <div className={styles.feedback_block}>
+          <div className={styles.feedback_first}>
+            <img className={styles.feedback_img} src={process.env.PUBLIC_URL + "img/feedback/mazda.png"} alt="" />
+            <p className={styles.car_information}>Mazda 3 2008 г.</p>
+            <img className={styles.feedback_avatar} src={process.env.PUBLIC_URL + "img/avatar/feedback.png"} alt="" />
+            <p className={styles.feedback}>Отзыв</p>
+            <div className={styles.feedback_div}>
+              <p className={styles.feedback_text}>Долго искал и т.д.</p>
+            </div>
+          </div>
+          <div className={styles.feedback_second}>
+            <img className={styles.feedback_img} src={process.env.PUBLIC_URL + "img/feedback/mazda.png"} alt="" />
+            <p className={styles.car_information}>Mazda 3 2008 г.</p>
+            <img className={styles.feedback_avatar} src={process.env.PUBLIC_URL + "img/avatar/feedback.png"} alt="" />
+            <p className={styles.feedback}>Отзыв</p>
+            <div className={styles.feedback_div}>
+              <p className={styles.feedback_text}>Долго искал и т.д.</p>
+            </div>
+          </div>
+          <div className={styles.feedback_third}>
+            <img className={styles.feedback_img} src={process.env.PUBLIC_URL + "img/feedback/mazda.png"} alt="" />
+            <p className={styles.car_information}>Mazda 3 2008 г.</p>
+            <img className={styles.feedback_avatar} src={process.env.PUBLIC_URL + "img/avatar/feedback.png"} alt="" />
+            <p className={styles.feedback}>Отзыв</p>
+            <div className={styles.feedback_div}>
+              <p className={styles.feedback_text}>Долго искал и т.д.</p>
+            </div>
+          </div>
+        </div>
+
+        <Information></Information>
+
+        <button>
+          <p>Оставить заявку для консультации</p>
+        </button>
+
+        <div className={styles.bottom}>
+          <p className={styles.p_bottom}>
+            Сайт является местом содержания краткой информации о услугах Иванова Ивана Ивановича и не содержит полной информации.
+            Для консультации вы сможете уточнить по номеру - +7 (999) 999-99-99
+          </p>
+          <p>При обнаружении проблем с сайтом сообщайте по почте - gmail@gmail.com</p>
+        </div>
       </div>
     </div>
   );
--- client/src/components/ui/toolbar/Toolbar.jsx
+++ client/src/components/ui/toolbar/Toolbar.jsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Toolbar.module.css";
 
 const Toolbar = () => {
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseOver = (e) => {
+    console.log(e);
+    setIsHovering(true);
+  };
+
+  const handleMouseOut = (e) => {
+    setIsHovering(false);
+  };
+
   return (
     <div>
       <div className={styles.header}>
         <p className={styles.name}>IVANOV</p>
         <p className={styles.number}>+7 (999) 999 99 99</p>
         <p className={styles.number_information}>(Скажите, что вы с сайта и получите скидку!)</p>
-        <button className={styles.services}>Услуги</button>
-        <button className={styles.about_me}>Обо мне</button>
-        <button className={styles.guarantees}>Гарантии</button>
-        <button className={styles.contacts}>Контакты</button>
+        <button onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className={styles.services}>
+          Услуги
+        </button>
+        <button onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className={styles.about_me}>
+          Обо мне
+        </button>
+        <button onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className={styles.guarantees}>
+          Гарантии
+        </button>
+        <button onMouseOver={handleMouseOver} onMouseOut={handleMouseOut} className={styles.contacts}>
+          Контакты
+        </button>
       </div>
     </div>
   );
